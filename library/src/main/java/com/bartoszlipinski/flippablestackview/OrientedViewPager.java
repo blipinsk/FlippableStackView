b118afa8fdb609fc3982762cf96bb4db973d3cf1
hesby <jobhesk@gmail.com>
2016-11-14 16:57:15 +0800

    update version
    update version

--- library/src/main/java/com/bartoszlipinski/flippablestackview/OrientedViewPager.java
+++ library/src/main/java/com/bartoszlipinski/flippablestackview/OrientedViewPager.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2015 Bartosz Lipinski
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -525,9 +525,9 @@ public class OrientedViewPager extends ViewGroup {
      * Set a {@link android.support.v4.view.ViewPager.PageTransformer} that will be called for each attached page whenever
      * the scroll position is changed. This allows the application to apply custom property
      * transformations to each page, overriding the default sliding look and feel.
-     * <p/>
-     * <p><em>Note:</em> Prior to Android 3.0 the property animation APIs did not exist.
-     * As a result, setting a PageTransformer prior to Android 3.0 (API 11) will have no effect.</p>
+     *
+     * <em>Note:</em> Prior to Android 3.0 the property animation APIs did not exist.
+     * As a result, setting a PageTransformer prior to Android 3.0 (API 11) will have no effect.
      *
      * @param reverseDrawingOrder true if the supplied PageTransformer requires page views
      *                            to be drawn from last to first instead of first to last.
@@ -600,16 +600,16 @@ public class OrientedViewPager extends ViewGroup {
      * Set the number of pages that should be retained to either side of the
      * current page in the view hierarchy in an idle state. Pages beyond this
      * limit will be recreated from the adapter when needed.
-     * <p/>
-     * <p>This is offered as an optimization. If you know in advance the number
+     *
+     * This is offered as an optimization. If you know in advance the number
      * of pages you will need to support or have lazy-loading mechanisms in place
      * on your pages, tweaking this setting can have benefits in perceived smoothness
      * of paging animations and interaction. If you have a small number of pages (3-4)
      * that you can keep active all at once, less time will be spent in layout for
-     * newly created view subtrees as the user pages back and forth.</p>
-     * <p/>
-     * <p>You should keep this limit low, especially if your pages have complex layouts.
-     * This setting defaults to 1.</p>
+     * newly created view subtrees as the user pages back and forth.
+     *
+     * You should keep this limit low, especially if your pages have complex layouts.
+     * This setting defaults to 1.
      *
      * @param limit How many pages will be kept offscreen in an idle state.
      */
@@ -2544,20 +2544,19 @@ public class OrientedViewPager extends ViewGroup {
 
     /**
      * Start a fake drag of the pager.
-     * <p/>
-     * <p>A fake drag can be useful if you want to synchronize the motion of the ViewPager
+     A fake drag can be useful if you want to synchronize the motion of the ViewPager
      * with the touch scrolling of another view, while still letting the ViewPager
      * control the snapping motion and fling behavior. (e.g. parallax-scrolling tabs.)
      * Call {@link #fakeDragBy(float)} to simulate the actual drag motion. Call
      * {@link #endFakeDrag()} to complete the fake drag and fling as necessary.
-     * <p/>
-     * <p>During a fake drag the ViewPager will ignore all touch events. If a real drag
+     During a fake drag the ViewPager will ignore all touch events. If a real drag
      * is already in progress, this method will return false.
      *
      * @return true if the fake drag began successfully, false if it could not be started.
      * @see #fakeDragBy(float)
      * @see #endFakeDrag()
      */
+
     public boolean beginFakeDrag() {
         if (mIsBeingDragged) {
             return false;
@@ -3324,7 +3323,10 @@ public class OrientedViewPager extends ViewGroup {
         /**
          * Factory method for {@link Parcelable.Creator}.
          *
+
+         *
          * @param callbacks Creator callbacks implementation.
+         * @param <T> item
          * @return New creator.
          */
         public static <T> Parcelable.Creator<T> newCreator(
